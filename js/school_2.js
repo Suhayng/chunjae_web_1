95cb545612b562cf66a6ae1783109cddc36c7497
Suhyang <znsl65@naver.com>
2024-03-03 12:13:23 +0900

    school2 스크롤 슬라이딩 추가

--- js/school_2.js
+++ js/school_2.js
@@ -1,7 +1,8 @@
+"use strict"
 /* 스크롤 애니메이션 */
 
 /* 배너 문구 애니메이션 */
-let observer = new IntersectionObserver((e) =>{
+let observer = new IntersectionObserver((e) => {
     // 감시중 박스가 화면에 등장하면 아래 코드 실행
     e.forEach((item) => {
         if (item.isIntersecting) {    // 화면에 보일때만 동작
@@ -9,7 +10,7 @@ let observer = new IntersectionObserver((e) =>{
         } else {
             item.target.style.opacity = 0;
         }
-        })
+    })
 })
 
 let inner = document.querySelectorAll("h2")
@@ -35,4 +36,28 @@ let saFunc = function () {
 window.addEventListener('load', saFunc);
 window.addEventListener('scroll', saFunc);
 
+/* 슬라이드 효과 */
+let slideIndex = 0;
+    showSlide();
+
+function showSlide() {
+    let slide = document.getElementsByClassName("slide");
+    let slide1 = document.getElementsByClassName("slide1");
+    let slide2 = document.getElementsByClassName("slide2");
+
+    for (let i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+        slide1[i].style.display = "none";
+        slide2[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slide.length) {
+        slideIndex = 1;
+    }
+    slide[slideIndex - 1].style.display = "block";
+    slide1[slideIndex - 1].style.display = "block";
+    slide2[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlide, 5000); //5초마다 이미지 변경
+}
 
